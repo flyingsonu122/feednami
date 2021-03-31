bb51ecc1004883507a7821138d611606628e169e
Sonu kumar kushwaha <flyingsonu736@gmail.com>
2021-03-31 21:41:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
-
-// var url = 'http://daringfireball.net/feeds/articles'
-// var url = 'https://jsbeginners.com/feed/'
-// var url = 'https://news.google.com/rss?hl=en-IN&gl=IN&ceid=IN%3Aen&oc=11'
-
 const textarea = document.querySelector('#feed-textarea > ul');
 
 const date = new Date();
 document.querySelector('#date').innerHTML = date.toDateString();
 
-var url = 'https://news.google.com/atom?hl=en-IN&gl=IN&ceid=IN%3Aen&oc=11'
+var url = 'https://news.google.com/rss?hl=en-IN&gl=IN&ceid=IN%3Aen&oc=11'
 feednami.load(url, function (result) {
     if (result.error) {
         console.log(result.error)
@@ -19,12 +14,17 @@ feednami.load(url, function (result) {
             var entry = entries[i]
 
             // console.log(entry.title)
+            // console.log(entry.summary)
 
             let li = document.createElement('li');
+            let para = document.createElement('p');
             //add HTML content to list items
             li.innerHTML = `<h4><a href="${entry.link}">${entry.title}</a></h4>`;
+            para.innerHTML = entry.summary;
             //append HTML content to list 
+
             textarea.appendChild(li);
+            textarea.appendChild(para);
         }
     }
 })
